08ba851e04c9608faca8c7d5c6a7bce52e092c3a
Irshad-Khan <mcsirshad95@gmail.com>
2022-12-12 18:26:46 +0500

    mongoose package installed and data inserted into mongo db

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
 const MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('mongodb://localhost:27017/animals', (err, db) => {
+MongoClient.connect('mongodb://localhost:27017/', (err, client) => {
   if (err) throw err
 
-//   db.collection('mammals').find().toArray((err, result) => {
-//     if (err) throw err
+  const db = client.db('animals')
 
-//     console.log(result)
-//   })
+    db.collection('mammals').insertOne({
+        name: "HORSE"
+    }, (err, result) => {
+        if (err) {
+            return err;
+        }
+
+        console.log("INSERTED");
+    });
 })
+
+
+
+
+
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost:27017/animals');
+// mongoose
+//     .connection
+//     .once('open', () => console.log('CONNECTED'))
+//     .on('error', (err) => {
+//         console.log('could not connect', err);
+//     });
